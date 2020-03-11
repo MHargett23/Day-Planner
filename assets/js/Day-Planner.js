74f0e33ef7719a1ef4e54a9ce909b8536f36905d
MHargett23 <51856965+MHargett23@users.noreply.github.com>
2020-03-11 19:15:32 -0400

    updated colors, buttons, added time stamp, and saves to local storage

--- assets/js/Day-Planner.js
+++ assets/js/Day-Planner.js
@@ -1,2 +1,26 @@
 var currentDateAndTime = Date(Date.now());//(Eastern Daylight Time)
 var currentHour = new Date().getHours();//Current hour in military time
+currentHour = 12
+$('.date').text(currentDateAndTime)
+for (let i = 9; i < 18; i++) {
+    if (currentHour > i) {
+        $('#h' + i).addClass('bg-secondary')
+    }
+    else if (currentHour === i) {
+        $('#h' + i).addClass('bg-danger')
+    }
+    else {
+        $('#h' + i).addClass('bg-success')
+    }
+    var text=localStorage.getItem('h'+i)
+    if(text){
+        $('#h'+i).val(text)
+    }
+}
+
+$(".submit").click(function () {
+    var hour = $(this).val()
+    var text = $("#h" + hour).val().trim()
+    console.log(hour, text, '<======>')
+    localStorage.setItem('h' + hour, text)
+})
